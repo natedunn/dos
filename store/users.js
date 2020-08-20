0b131103462b0be65e8bcd102bc28640996f3f68
Nate Dunn <hello@natedunn.net>
2020-08-20 00:40:46 -0500

    Add ability to get and mutate activeUser

--- store/users.js
+++ store/users.js
@@ -1,3 +1,5 @@
+import * as types from './mutation-types'
+
 export const state = () => ({
   users: [
     {
@@ -9,8 +11,22 @@ export const state = () => ({
       id: '5678',
     },
   ],
+  activeUser: '5678',
 })
 
 export const getters = {
   users: (state) => state.users,
+  activeUser: (state) => state.activeUser,
+}
+
+export const mutations = {
+  [types.UPDATE_ACTIVE_USER](state, id) {
+    state.activeUser = id
+  },
+}
+
+export const actions = {
+  updateActiveUser({ commit }, payload) {
+    commit(types.UPDATE_ACTIVE_USER, payload)
+  },
 }
