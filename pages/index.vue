4b314b313d196e8a4b80d64580e98ec90d077a54
Nate Dunn <hello@natedunn.net>
2020-08-19 22:42:20 -0500

    Remove header for layout reasons for now

--- pages/index.vue
+++ pages/index.vue
@@ -1,14 +1,13 @@
 <template>
   <div class="flex flex-col h-screen bg-cool-gray-200">
-    <div
-      class="flex items-center justify-between px-4 py-4 text-white bg-cool-gray-900"
+    <!-- <div
+      class="flex items-center justify-between px-6 py-4 text-white bg-cool-gray-900"
     >
       <div>
         <span class="font-bold">JourneyChat</span>
       </div>
       <div>Menu</div>
-    </div>
-    <div class="grid flex-auto grid-cols-1 gap-4 px-4 py-4 md:grid-cols-2">
+    </div> -->
       <chat-wrapper v-for="user in users" :key="user.id" :user="user" />
     </div>
   </div>
