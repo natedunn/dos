78fb55fc4051a91ae2082f473f2073e203feb0ab
Nate Dunn <hello@natedunn.net>
2020-08-19 22:46:11 -0500

    Break section into new component

---
+++ components/ChatHeader.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="px-4 py-4 font-bold bg-white border-b border-cool-gray-200">
+    {{ user.name }}
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    user: {
+      type: Object,
+      required: true,
+    },
+  },
+}
+</script>
+
+<style></style>
