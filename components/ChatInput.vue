bf113f443e92c76d8d5463dde04b1e9b1e08e0d9
Nate Dunn <hello@natedunn.net>
2020-08-20 02:11:57 -0500

    Update placeholder text

--- components/ChatInput.vue
+++ components/ChatInput.vue
@@ -3,7 +3,7 @@
     <input
       class="inline-flex w-full px-4 py-2 text-base leading-6 text-gray-700 border-2 border-gray-400 rounded-full appearance-none focus:z-0 focus:outline-none focus:bg-white focus:border-red-600 focus:shadow-outline-red"
       type="text"
-      :placeholder="`Posting as ${userId}`"
+      placeholder="Type a message"
       @keydown.enter="submit"
     />
   </div>
