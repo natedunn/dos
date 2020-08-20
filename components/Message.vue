1fbfe65561ecd6e7e8691f7a0026d3b2d3ea5abf
Nate Dunn <hello@natedunn.net>
2020-08-19 22:45:02 -0500

    Add message classes + stylings

--- components/Message.vue
+++ components/Message.vue
@@ -19,11 +19,9 @@ export default {
   },
   computed: {
     classes() {
-      const bothClasses = ' rounded-full max-w-lg py-4 px-5 mt-6 mx-6'
-      const ownClasses =
-        'bg-teal-300 text-gray-900 rounded-br-none ml-auto' + bothClasses
-      const otherClasses =
-        'bg-gray-200 text-gray-900 rounded-bl-none mr-auto' + bothClasses
+      const bothClasses = 'rounded-full max-w-lg py-3 px-4 mt-3 mx-6'
+      const ownClasses = `bg-journey-red text-white font-medium rounded-tr-none ml-auto ${bothClasses} own-message`
+      const otherClasses = `bg-gray-200 text-gray-900 font-medium mr-auto rounded-tl-none ${bothClasses} other-message`
 
       return this.own ? ownClasses : otherClasses
     },
@@ -31,4 +29,9 @@ export default {
 }
 </script>
 
-<style></style>
+<style lang="postcss">
+.own-message + .own-message,
+.other-message + .other-message {
+  @apply rounded-full;
+}
+</style>
